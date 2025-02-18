11ac90599954d3264009344d7c4d6e38c30c27dc
Lee Copeman <leepcopeman@gmail.com>
2025-02-18 16:13:11 +0000

    BUILD:Added profile views and form

---
+++ profiles/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Profile
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user',)
+           
+        
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_address': 'Street Address',           
+            'default_town_or_city': 'Town or City',  
+            'default_postcode': 'Post Code',
+            'default_county': 'County',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'rounded-0'
+            self.fields[field].label = False              
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import Profile
+from .forms import ProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(Profile, user=request.user)
 
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+
+    form = ProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile' : profile,
+        'form' : form,
+        'orders' : orders,
     }
 
     return render(request, template, context)
